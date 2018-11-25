766e438262f7dadee29a514c29f025a4756bffc2
Ivan Notaros <nothke@gmail.com>
2018-11-25 11:35:07 +0100

    Minima neighbors

--- Assets/Scripts/FirstPuddler.cs
+++ Assets/Scripts/FirstPuddler.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
+﻿using Nothke.Math.Coord;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using Nothke.Math.Coord;
 
 public class FirstPuddler : MonoBehaviour
 {
@@ -14,36 +13,116 @@ public class FirstPuddler : MonoBehaviour
         StartCoroutine(Co());
     }
 
+    const int size = 32;
+    bool[,] filled;
+
+    float H(Coord c)
+    {
+        return heights[c.x, c.y];
+    }
+
+    bool F(Coord c)
+    {
+        return filled[c.x, c.y];
+    }
+
+    bool Within(Coord c)
+    {
+        return c.x >= 0 && c.x < size && c.y >= 0 && c.y < size;
+    }
+
+    List<Coord> Neighbors(Coord c)
+    {
+        List<Coord> neighbors = new List<Coord>();
+        Coord c0 = c + new Coord(-1, -1);
+        Coord c1 = c + new Coord(0, -1);
+        Coord c2 = c + new Coord(1, -1);
+        Coord c3 = c + new Coord(-1, 0);
+        Coord c4 = c + new Coord(1, 0);
+        Coord c5 = c + new Coord(-1, 1);
+        Coord c6 = c + new Coord(0, 1);
+        Coord c7 = c + new Coord(1, 1);
+
+        if (Within(c0)) neighbors.Add(c0);
+        if (Within(c1)) neighbors.Add(c1);
+        if (Within(c2)) neighbors.Add(c2);
+        if (Within(c3)) neighbors.Add(c3);
+        if (Within(c4)) neighbors.Add(c4);
+        if (Within(c5)) neighbors.Add(c5);
+        if (Within(c6)) neighbors.Add(c6);
+        if (Within(c7)) neighbors.Add(c7);
+
+        return neighbors;
+    }
+
     IEnumerator Co()
     {
         // fill heights
-        heights = new float[32, 32];
+        heights = new float[size, size];
 
-        for (int x = 0; x < 32; x++)
+        for (int x = 0; x < size; x++)
         {
-            for (int y = 0; y < 32; y++)
+            for (int y = 0; y < size; y++)
             {
                 heights[x, y] = Mathf.PerlinNoise(x * 0.232f, y * 0.232f);
                 heights[x, y] += Mathf.PerlinNoise(x * 0.312f, y * 0.3243f);
             }
         }
 
+        filled = new bool[size, size];
         List<Coord> minima = new List<Coord>();
 
-        for (int x = 1; x < 31; x++)
+        for (int x = 1; x < size - 1; x++)
         {
-            for (int y = 1; y < 31; y++)
+            for (int y = 1; y < size - 1; y++)
             {
                 int highers = 0;
 
                 float h = heights[x, y];
-                if (h < heights[x + 1, y]) highers++;
-                if (h < heights[x - 1, y]) highers++;
-                if (h < heights[x, y + 1]) highers++;
-                if (h < heights[x, y - 1]) highers++;
+                if (h <= heights[x + 1, y]) highers++;
+                if (h <= heights[x - 1, y]) highers++;
+                if (h <= heights[x, y + 1]) highers++;
+                if (h <= heights[x, y - 1]) highers++;
 
-                if (highers == 4) minima.Add(new Coord(x, y));
+                if (highers == 4)
+                {
+                    filled[x, y] = true;
+                    minima.Add(new Coord(x, y));
+                }
             }
+        }
+
+        List<Coord> saddles = new List<Coord>();
+
+        foreach (var c in minima)
+        {
+            bool issaddle = false;
+            var neis = Neighbors(c);
+
+            float h = H(c);
+
+            // Fill neighbors
+            foreach (var nei in neis)
+            {
+                filled[nei.x, nei.y] = true;
+            }
+
+            /*
+            foreach (var nei in neis)
+            {
+                var neineis = Neighbors(nei);
+
+                filled[nei.x, nei.y] = true;
+
+                foreach (var neineis in collection)
+                {
+
+                }
+
+                if (H(nei) < h) issaddle = true;
+            }
+
+            if (issaddle) saddles.Add(c);*/
         }
 
         yield return null;
@@ -56,25 +135,36 @@ public class FirstPuddler : MonoBehaviour
                 Debug.DrawRay(new Vector3(min.x, 0, min.y), Vector3.up * 2, Color.red);
             }
 
+            // Debug fills
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    if (!filled[x, y]) continue;
+                    Vector3 p0 = new Vector3(x, heights[x, y], y);
+                    Debug.DrawRay(p0, Vector3.up * 5, Color.green);
+                }
+            }
+
             yield return null;
         }
     }
 
     private void Update()
     {
-        for (int x = 0; x < 32; x++)
+        for (int x = 0; x < size; x++)
         {
-            for (int y = 0; y < 32; y++)
+            for (int y = 0; y < size; y++)
             {
                 Vector3 p0 = new Vector3(x, heights[x, y], y);
 
-                if (x < 32 - 1)
+                if (x < size - 1)
                 {
                     Vector3 pr = new Vector3(x + 1, heights[x + 1, y], y);
                     Debug.DrawLine(p0, pr);
                 }
 
-                if (y < 32 - 1)
+                if (y < size - 1)
                 {
                     Vector3 pf = new Vector3(x, heights[x, y + 1], y + 1);
                     Debug.DrawLine(p0, pf);
